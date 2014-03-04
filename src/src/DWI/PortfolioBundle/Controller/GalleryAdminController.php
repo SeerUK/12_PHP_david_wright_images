1d12e61dd3fcea37ab67270fc676de794f077caa
Elliot Wright <ewright@ebuyer.com>
2014-03-04 18:45:40 +0000

    Gateway for sorting should work... maybe.

--- src/src/DWI/PortfolioBundle/Controller/GalleryAdminController.php
+++ src/src/DWI/PortfolioBundle/Controller/GalleryAdminController.php
@@ -13,6 +13,7 @@ namespace DWI\PortfolioBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Doctrine\ORM\NoResultException;
+use DWI\CoreBundle\HttpFoundation\RestJsonResponse;
 use DWI\PortfolioBundle\Entity\CoverImage;
 use DWI\PortfolioBundle\Entity\Gallery;
 use DWI\PortfolioBundle\Entity\Image;
@@ -56,5 +57,42 @@ class GalleryAdminController extends Controller
         return $this->render('DWIPortfolioBundle:Portfolio/Admin:gallery-manage.html.twig', array(
             'model' => $mp->prepareView(),
         ));
+    }
+
+    /**
+     * Sort Gallery Images
+     *
+     * @param  integer $id
+     * @param  array  $order
+     * @return Symfony\Component\HttpFoundation\Response
+     *
+     * @throws Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     * @throws Symfony\Component\Security\Core\Exception\AccessDeniedException
+     */
+    public function sortAction($id)
+    {
+        $request  = $this->get('request');
+        $response = new RestJsonResponse();
+
+        if ( ! $this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            return $response->addError(RestJsonResponse::GENERIC_BAD_AUTH)
+                ->setStatusCode(403);
+        } elseif ('POST' !== $request->getMethod()) {
+            return $response->addError(RestJsonResponse::BAD_REQUEST_METHOD)
+                ->setStatusCode(400);
+        }
+
+        $gr = $this->get('dwi_portfolio.gallery_repository');
+        $ig = $this->get('dwi_portfolio.image_gateway');
+
+        try {
+            $gallery = $gr->findById($id);
+        } catch (NoResultException $e) {
+            throw $this->createNotFoundException('That gallery doesn\'t exist!');
+        }
+
+        $response->setData($request->get('order'));
+
+        return $response;
     }
 }
